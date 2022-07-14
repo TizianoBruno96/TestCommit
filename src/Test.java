a4687aa2844b72a50dff7006068518c1872fdb81
Tiziano75775 <brntiziano9620@gmail.com>
2022-07-14 10:39:18 +0200

    Test

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,10 @@
 public class Test {
+    public static void main(String[] args) {
+        System.out.println(saluter());
+    }
+
+    public static String saluter() {
+        String s = "Hello world!";
+        return s;
+    }
 }
